e53baf935a2cfb55b3aa174b40753c3f4ab74ed2
Alexandr-K13 <sanni3680@mail.ru>
2020-07-28 08:12:01 +0400

    Inital commit

---
+++ src/Java_level_1_homework_1/Main.java
@@ -0,0 +1,35 @@
+package Java_level_1_homework_1;
+
+//public static void main (String[] args){
+//    int a = 120;
+//    int b = 15;
+//    float c =50,5f;
+//        int d = 30;
+//        float = (a*(b+(c/d))
+//    }
+//}
+
+//public static void main (String [] args){
+//    int a = 5;
+//    int b =10;
+//    int c = a + b;
+//    if ( c = 10; c<20; c+1);
+
+        //}
+
+
+//private static void cheknumber(int x){
+//        int x=10;
+//        if(int x< 0){
+//        System.out.println("X less then zero");
+//        }else
+//        System.out.println("X more then zero");
+//        }
+//}
+
+
+private static long hello user (String name){
+    returne name;
+    longe name  = hello user (Alexandr);
+    long name = hello user (Dmitriy);
+        }
